ebac1797f100e24d7b3ae669170c679e81d36641
suhana choudhary <suhanachaudhary212@gmail.com>
2025-03-30 20:37:42 +0530

    Mongodb setup

--- backend/index.js
+++ backend/index.js
@@ -22,10 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
